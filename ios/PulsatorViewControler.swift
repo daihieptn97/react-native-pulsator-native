d960f337a25112f1b83d6d1431b5e801b7ecdfa1
hiep <daihieptn97@gmail.com>
2020-08-25 23:42:37 +0700

    fix UIcontroler

--- ios/PulsatorViewControler.swift
+++ ios/PulsatorViewControler.swift
@@ -9,56 +9,66 @@
 import Foundation
 import Pulsator
 
-class PulsatorViewControler: UIView {
+class PulsatorViewControler: UIControl {
     var radarState = false
     let pulsator = Pulsator()
     
-    
+    override func layoutSubviews() {
+        super.layoutSubviews();
+        
+        
+        //        setUpPulsator()
+        
+        
+        let widthCircle = bounds.size.width * 0.3;
+        let x = bounds.size.width / 2 - widthCircle / 2
+        let y = bounds.size.height / 2 - widthCircle / 2
+        print("frame.size.width", bounds.size.width, bounds.size.height)
+        print("widthCircle x y", widthCircle, x, y)
+        
+        buttonT.frame = CGRect(x: x, y: y, width: widthCircle, height: widthCircle)
+        buttonT.layer.cornerRadius = (widthCircle * 0.5)
+        //buttonT.center = center
+        setUpPulsator()
+        
+    }
     private let buttonT : UIButton = {
         let btn = UIButton();
-        //        btn.setTitle("hello", for: .normal)
         btn.backgroundColor = UIColor(red: 98/255, green: 0/255, blue: 243/255, alpha: 1)
         btn.setTitleColor(.black, for: .normal)
         btn.layer.masksToBounds = true;
         btn.titleLabel?.font = .systemFont(ofSize: 20, weight : .bold);
-        
         //        Start Icon
-        btn.setImage( UIImage(named: "search"), for: .normal)
+        //        btn.setImage( UIImage(named: "search"), for: .normal)
         //        End setup icon
-        
         return btn;
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setUpPulsator()
-        self.addSubview(buttonT)
         
-        let widthCircle = self.frame.width * 0.3;
-        buttonT.frame = CGRect(x: 30, y: 30, width:widthCircle, height:widthCircle);
-        buttonT.layer.cornerRadius = (widthCircle * 0.5)
-        buttonT.center = self.center
         
         buttonT.addTarget(self, action: #selector(onButtonPressed), for: .touchUpInside)
+        self.addSubview(buttonT)
+        
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
     }
-    
-
-    
+  
     private func setUpPulsator() {
-        pulsator.frame = CGRect(x: self.frame.width / 2, y: self.frame.height / 2, width: 0, height: 0)
+        pulsator.frame = CGRect(x: bounds.size.width / 2 , y: bounds.size.height / 2 , width: 0, height: 0)
         self.layer.addSublayer(pulsator)
         pulsator.numPulse = 10
         pulsator.radius = 200.0
         pulsator.backgroundColor = UIColor(red: 98/255, green: 0/255, blue: 243/255, alpha: 1).cgColor
-        pulsator.animationDuration = 6.5
+        pulsator.animationDuration = 6.0
         pulsator.pulseInterval = 1.0
     }
     
     @objc func onButtonPressed() {
+        
         if radarState {
             pulsator.stop()
             radarState = false
@@ -67,6 +77,7 @@ class PulsatorViewControler: UIView {
             radarState = true
         }
     }
+    
     
 }
 
