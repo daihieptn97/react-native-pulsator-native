8b2af5b46d6983908e9ba2fe965af2d0e310c3a3
hieptran <daihieptn97@gmail.com>
2020-09-05 09:32:12 +0700

    fix bug styles

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-import { requireNativeComponent, View, TouchableOpacity, StyleSheet } from 'react-native';
+import { requireNativeComponent, View, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
 import React from 'react';
 
 const PulsatorNative = requireNativeComponent('PulsatorNative', null);
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 let stylesDefault = {
-  width: 300,
-  height: 300,
+  width: SCREEN_WIDTH * 0.8,
+  height: SCREEN_WIDTH * 0.8,
   justifyContent: 'center',
   alignItems: 'center',
   zIndex: -1,
 };
 
 
-export default function ({ pulsatorColor = '#08400d', styles = stylesDefault, children, isRunning }) {
+export default function({ pulsatorColor = '#08400d', styles = stylesDefault, children, isRunning }) {
+  console.log(styles);
   return <PulsatorNative
-    style={[stylesDefault, styles]}
+    style={[styles]}
     pulsatorColor={pulsatorColor}
     isRunning={isRunning}
   >
-    <View style={{ zIndex: 1 }}>
+    <View style={{
+      zIndex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      justifyItems: 'center',
+      flex: 1,
+    }}>
       {children}
     </View>
 
